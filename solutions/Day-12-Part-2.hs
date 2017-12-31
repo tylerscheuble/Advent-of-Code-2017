5e12fcc84fc8f6083b2516b5775a39979b81120c
tylerscheuble <tylerscheuble@gmail.com>
2017-12-30 17:23:35 -0800

    Day 12

---
+++ solutions/Day-12-Part-2.hs
@@ -0,0 +1,26 @@
+import Data.Graph (Graph, Vertex, graphFromEdges, dff)
+import System.IO.Unsafe (unsafePerformIO)
+
+puzzleInput = unsafePerformIO $ readFile "./Day-12-Input.txt"
+
+type Program = (Int, Int, [Int])
+
+connection :: String -> Int
+connection s = read [ c | c <- s, c /= ',' ]
+
+program :: String -> Program
+program s = let ws = words s
+                id = read $ head ws
+                connections = map connection $ drop 2 ws in
+                (id, id, connections)
+
+solve :: String -> Int
+solve = length
+    . dff
+    . (\(x, _, _) -> x)
+    . graphFromEdges . map program . lines
+
+solution = solve puzzleInput
+
+main = print solution
+
