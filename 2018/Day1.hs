6e4bddfa4a30e2de5c08b58f15a5c44060ee2133
tylerscheuble <tyler@scheuble.us>
2018-12-02 02:10:48 -0800

    Finish day 1

--- 2018/Day1.hs
+++ 2018/Day1.hs
@@ -1,18 +1,47 @@
 import           Data.Foldable    (foldMap)
+import           Data.Maybe       (fromJust)
 import           Data.Monoid
+import           Data.Set         as Set
 import           System.IO.Unsafe (unsafePerformIO)
 
 input :: String
-input = unsafePerformIO $readFile "./Day-1-Input.txt"
+input = unsafePerformIO $ readFile "./Day1-Input.txt"
 
 -- read can't handle unary plus
 readFrequency :: String -> Sum Int
-readFrequency ('+':xs) = Sum (read xs)
-readFrequency x        = Sum (read x)
+readFrequency ('+':xs) = Sum $ read xs
+readFrequency x        = Sum $ read x
+
+repeating :: [a] -> [a]
+repeating = concat . repeat
+
+concatenations :: Monoid m => m -> [m] -> [m]
+concatenations p []     = [p]
+concatenations p (x:xs) = p:(concatenations (p <> x) xs)
+
+firstDup :: Ord a => [a] -> Maybe a
+firstDup = firstDup' empty
+  where firstDup' :: Ord a => Set a -> [a] -> Maybe a
+        firstDup' _ [] = Nothing
+        firstDup' prev (x:xs)
+          | member x prev = Just x
+          | otherwise = firstDup' (insert x prev) xs
 
 solve1 :: String -> Int
-solve1 = getSum . foldMap readFrequency  . lines
+solve1 = getSum
+  . foldMap readFrequency
+  . lines
+
+solve2 :: String -> Int
+solve2 = fromJust
+  . firstDup
+  . fmap getSum
+  . concatenations mempty
+  . repeating
+  . fmap readFrequency
+  . lines
 
 main :: IO ()
 main = do
   print $ solve1 input
+  print $ solve2 input
