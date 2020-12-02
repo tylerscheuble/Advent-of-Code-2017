b961864ae3cbf39b1397c4445470402e916803ec
tylerscheuble <5944807+tylerscheuble@users.noreply.github.com>
2020-12-01 23:07:45 -0800

    Day 1 part 1

--- 2020/src/aoc2020/day1/solution.py
+++ 2020/src/aoc2020/day1/solution.py
@@ -1,4 +1,6 @@
+import bisect
 import logging
+from typing import List
 
 from aoc2020.common.input import load_input
 
@@ -7,9 +9,29 @@ logger = logging.getLogger(__name__)
 DAY = 1
 
 
+def parse_input(str) -> List[int]:
+    return [int(x) for x in str.splitlines()]
+
+
 def solve_part_1() -> str:
-    inpt = load_input(DAY)  # noqa: F841
-    raise NotImplementedError
+    entries = sorted(parse_input(load_input(DAY)))
+
+    # NOTE: This could break if there is one entry of 1010
+    # (half of 2020). I'm going to ignore that case as
+    # it is not in my input.
+    assert not any(x == 1010 for x in entries)
+
+    for i, entry in enumerate(entries):
+        target = 2020 - entry
+
+        # a binary search against each entry gives us
+        # O(n log n) instead of O(n^2)
+        closest = entries[bisect.bisect_left(entries, target)]
+
+        if closest == target:
+            return str(entry * closest)
+
+    raise ValueError("No solution found")
 
 
 def solve_part_2() -> str:
